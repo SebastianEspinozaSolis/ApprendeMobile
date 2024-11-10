b5aae3cd1e60a5385fa49a70b2a85bc520d68e10
Helmutdo <helmut.schweitzerg@gmail.com>
2024-11-10 17:35:25 -0300

    menu_alumno boton_pomodoro link a sitio web pomodoro.io

--- app/src/main/java/cl/grupo_uno/apprende/MenuAlumno.java
+++ app/src/main/java/cl/grupo_uno/apprende/MenuAlumno.java
@@ -1,6 +1,7 @@
 package cl.grupo_uno.apprende;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,15 +17,18 @@ public class MenuAlumno extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu_alumno);
 
-        Button botonPomodoro = (Button) findViewById(R.id.botonPomodoro);
+        // Referencia al botón
+        Button botonPomodoro = findViewById(R.id.botonPomodoro);
 
+        // Configurar el evento de clic en el botón
         botonPomodoro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MenuAlumno.this, Pomodoro.class));
+                // Intent para abrir el navegador con la URL de Pomofocus
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://pomofocus.io/"));
+                startActivity(intent);
             }
         });
     }
